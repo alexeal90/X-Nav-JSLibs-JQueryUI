756f51e8e4cee481a36b377f7b7d9f3c12b0803d
alexeal90 <alexeal90@gmail.com>
2016-04-20 01:18:06 +0200

    Ejercicio jQuery IU

--- index.js
+++ index.js
@@ -6,9 +6,60 @@ $(document).ready(function(){
   		inline: true
   	});
 
+
+    $(function() {
+	    $( "#textout" ).dialog({
+	      autoOpen: false,
+	      show: {
+	        effect: "bounce",
+	        duration: 1000
+	      },
+	    });
+	    $( "#logout" ).click(function() {
+	      $( "#textout" ).dialog( "open" );
+	    });
+    });
+
   	$( ".draggable" ).draggable({stack: "#droppable"});
-  	$( ".draggable" ).resizable({animate: true, autoHide: true, ghost: true});
-    $( "#droppable" ).resizable({animate: true, autoHide: true, ghost: true});
+  	$( "#draggable1" ).resizable({
+      animate: true,
+      autoHide: true,
+      ghost: true,
+      minWidth: 180,
+      resize: function(){
+        $(".texto1").css({
+               position:'absolute',
+               top: ($("#draggable1").height() - $(".texto1").outerHeight())
+          });
+      }
+    });
+
+    $( "#draggable2" ).resizable({
+      animate: true,
+      autoHide: true,
+      ghost: true,
+      minWidth: 180,
+      resize: function(){
+        $(".texto2").css({
+               position:'absolute',
+               top: ($("#draggable2").height() - $(".texto2").outerHeight())
+          });
+      }
+    });
+
+    $( "#draggable3" ).resizable({
+      animate: true,
+      autoHide: true,
+      ghost: true,
+      minWidth: 180,
+      resize: function(){
+        $(".texto3").css({
+               position:'absolute',
+               top: ($("#draggable3").height() - $(".texto3").outerHeight())
+          });
+      }
+    });
+    /*$( "#droppable" ).resizable({animate: true, autoHide: true, ghost: true});*/
   	$( "#droppable" ).droppable({
   		over: function( event, ui ) {
   				$( this )
